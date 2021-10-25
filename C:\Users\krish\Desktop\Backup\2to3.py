be1c746eff7196829dccbade560b991e932841cf
krishrajchal <67868262+krishrajchal@users.noreply.github.com>
2021-10-25 15:43:07 -0600

    A .py in my computer

---
+++ C:\Users\krish\Desktop\Backup\2to3.py
@@ -0,0 +1,7 @@
+# Date created: Thu Oct 21 15:34:26 2021
+# Last date modified: Sat Jul 24 17:17:04 2021
+#!/usr/bin/env python
+import sys
+from lib2to3.main import main
+
+sys.exit(main("lib2to3.fixes"))
