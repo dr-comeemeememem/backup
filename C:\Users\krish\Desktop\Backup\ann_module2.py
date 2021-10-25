9cd7d8f42ad99e7bd02850165d581f958ee7846a
krishrajchal <67868262+krishrajchal@users.noreply.github.com>
2021-10-25 15:43:15 -0600

    A .py in my computer

---
+++ C:\Users\krish\Desktop\Backup\ann_module2.py
@@ -0,0 +1,38 @@
+# Date created: Thu Oct 21 15:34:26 2021
+# Last date modified: Sat Jul 24 17:17:04 2021
+"""
+Some correct syntax for variable annotation here.
+More examples are in test_grammar and test_parser.
+"""
+
+from typing import no_type_check, ClassVar
+
+i: int = 1
+j: int
+x: float = i/10
+
+def f():
+    class C: ...
+    return C()
+
+f().new_attr: object = object()
+
+class C:
+    def __init__(self, x: int) -> None:
+        self.x = x
+
+c = C(5)
+c.new_attr: int = 10
+
+__annotations__ = {}
+
+
+@no_type_check
+class NTC:
+    def meth(self, param: complex) -> None:
+        ...
+
+class CV:
+    var: ClassVar['CV']
+
+CV.var = CV()
