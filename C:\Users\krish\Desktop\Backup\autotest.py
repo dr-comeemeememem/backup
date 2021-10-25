fae15912fc620673d100dd6228849e1827b4d15c
krishrajchal <67868262+krishrajchal@users.noreply.github.com>
2021-10-25 15:43:22 -0600

    A .py in my computer

---
+++ C:\Users\krish\Desktop\Backup\autotest.py
@@ -0,0 +1,7 @@
+# Date created: Thu Oct 21 15:34:26 2021
+# Last date modified: Sat Jul 24 17:17:04 2021
+# This should be equivalent to running regrtest.py from the cmdline.
+# It can be especially handy if you're in an interactive shell, e.g.,
+# from test import autotest.
+from test.libregrtest import main
+main()
